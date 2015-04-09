2ef8cc4e754d8915c21eab106ea3aa0e359a6184
root <root@iZ25onkk4uyZ.(none)>
2015-04-09 16:05:23 +0800

    Update

--- mkblog/wsgi.py
+++ mkblog/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join,dirname,abspath
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mkblog.settings")
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))#3
+import sys # 4
+sys.path.insert(0,PROJECT_DIR) # 5
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
